e40f71cbb8eaf281ebd89b1826beb7c162cb78ff
Larissa <larissasousa.wrks@gmail.com>
2024-01-25 15:38:27 -0400

    Riddle project pt2

--- img/JS Game_files/app.js
+++ img/JS Game_files/app.js
@@ -1,22 +1,40 @@
-// let numeroSecreto = parseInt(Math.random() * 11)
-// let tentativas = 1
-// let chute
+// alert("Boas vindas ao jogo do número secreto");
+// let numeroMaximo = 5000;
+// //numero aleatório e inteiro entre 1 e 10 onde o 10 corresponde ao range e o +1 foi adicionado pq o padrão é 0-99)
+// let numeroSecreto = parseInt(Math.random()*numeroMaximo+1);
+// let chute;
+// let tentativas = 1;
 
-// alert('Boas vindas ao jogo do número secreto')
-
+// //enquanto o chute não for o numero secreto
 // while (chute != numeroSecreto) {
-//     let chute = prompt('Escolha um número entre 1 e 10')
-//     if (chute == numeroSecreto) {
-//         break
+//   chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+//   //se chute = numeroSecreto
+//   if (chute == numeroSecreto) {
+//     break;
+//   } else {
+//     if (chute > numeroSecreto) {
+//       alert(`O numero secreto é menor que o ${chute}`);
 //     } else {
-//         if (chute > numeroSecreto) {
-//             alert('O número secreto é menor')
-//         } else {
-//             alert('O número secreto é maior')
-//         }
+//       alert(`O numero secreto é maior que o ${chute}`);
 //     }
-//     tentativas++
+//     //tentativas = tentativas+1
+//     tentativas++;
+//   }
 // }
+// //operador ternario
+// let flexaoDeNumero = tentativas > 1 ? 'tentativas' : 'tentativa';
+//   alert(
+//     `Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${flexaoDeNumero}`
+//   );
 
-// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
-// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
+// if (tentativas > 1) {
+//   alert(
+//     `Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`
+//   );
+// } else {
+//   {
+//     alert(
+//       `Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`
+//     );
+//   }
+// }
