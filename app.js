2e1c38577499be4eed3d372a2c243ed2cc39997d
Larissa <larissasousa.wrks@gmail.com>
2024-03-11 20:21:57 -0400

    updating new challenges

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+let secretNumber = generateRandomNumber();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function showTextOnScreen(tag, text){
+  let field = document.querySelector(tag);
+  field.innerHTML = text;
+}
 
-console.log(paragrafo);
-function verificarChute(){
-  
+showTextOnScreen('h1','Jogo do Número Secreto');
+showTextOnScreen('p','Escolha um número entre 1 e 10');
+
+function verifyGuess(){
+  let guess = document.querySelector('input').value;
+  console.log(guess == secretNumber);
 };
+
+
+function generateRandomNumber(){
+  return parseInt(Math.random() * 10 + 1)
+}
