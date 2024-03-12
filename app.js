ea6b6e1adfa9a34b44b63b98daea7bac6a9dc55e
Larissa <larissasousa.wrks@gmail.com>
2024-03-11 23:13:15 -0400

    refactoring in progress - new button params and alerts

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
 let secretNumber = generateRandomNumber();
+let attempts = 1;
 
-function showTextOnScreen(tag, text){
+function showTextOnScreen(tag, text) {
   let field = document.querySelector(tag);
   field.innerHTML = text;
 }
 
-showTextOnScreen('h1','Jogo do Número Secreto');
-showTextOnScreen('p','Escolha um número entre 1 e 10');
+function showInitialMsg(){
+  showTextOnScreen("h1", "Jogo do Número Secreto");
+  showTextOnScreen("p", "Escolha um número entre 1 e 10");
+}
 
-function verifyGuess(){
-  let guess = document.querySelector('input').value;
-  console.log(guess == secretNumber);
-};
+showInitialMsg();
 
 
-function generateRandomNumber(){
-  return parseInt(Math.random() * 10 + 1)
+function verifyGuess() {
+  let guess = document.querySelector("input").value;
+  // console.log(guess == secretNumber);
+
+  if (guess == secretNumber) {
+    showTextOnScreen("h1", "Acertou");
+    let nOfAttempts = attempts > 1 ? "tentativas" : "tentativa";
+    let guessedRight = `Isso aí! Você descobriu o número secreto com ${attempts} ${nOfAttempts}!`;
+    showTextOnScreen('p',guessedRight);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else {
+    if (guess > secretNumber) {
+      showTextOnScreen("h1", "Errou");
+      showTextOnScreen("p", `O numero secreto é menor que ${guess}`);
+    } else {
+      showTextOnScreen("h1", "Errou");
+      showTextOnScreen("p", `O numero secreto é maior que ${guess}`);
+    }
+    attempts++;
+    cleanField();
+  }
+}
+
+function generateRandomNumber() {
+  return parseInt(Math.random() * 10 + 1);
+}
+
+function cleanField() {
+  guess = document.querySelector('input');
+  guess.value = '';
+}
+
+function resetGame(){
+  secretNumber = generateRandomNumber();
+  cleanField();
+  attempts = 1;
+  showInitialMsg();
+  document.getElementById('reiniciar').setAttribute('disabled',true);
 }
